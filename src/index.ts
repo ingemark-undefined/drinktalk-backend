16a658e9b4c01676cd72e96b83d3665b338af405
fifi98 <bel.filip@icloud.com>
2021-09-20 14:27:10 +0200

    chore: add socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello");
+import { Server, Socket } from "socket.io";
+import { createServer } from "http";
+
+const httpServer = createServer();
+const io = new Server(httpServer);
+
+io.on("connection", (socket: Socket) => {
+  console.log("connection");
+});
+
+httpServer.listen(4000);
