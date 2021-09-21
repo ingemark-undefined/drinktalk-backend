511e10ef27edb52064387e235fa6a021b07a6888
fifi98 <bel.filip@icloud.com>
2021-09-21 16:36:04 +0000

    style: export handlers and misc in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,20 @@
-import { Socket } from "socket.io";
-import { createServer } from "http";
+import { Socket } from 'socket.io';
 
-import newGame from "@handlers/newGame";
-import joinGame from "./handlers/joinGame";
-import startGame from "./handlers/startGame";
-import lostGame from "./handlers/lostGame";
-import onDisconnect from "./handlers/onDisconnect";
-import rooms from "./misc/storage";
-import io from "./misc/server";
+import { newGame, joinGame, startGame, lostGame, onDisconnect } from '@handlers/index';
+import { rooms, io } from '@misc/index';
 
 const onConnection = (socket: Socket) => {
   socket.data.user = socket.handshake.auth.user;
 
-  socket.on("new", (time) => newGame(socket, time));
-  socket.on("join", (gameId) => joinGame(socket, gameId));
-  socket.on("start", () => startGame(socket));
-  socket.on("lost", () => lostGame(socket));
-  socket.on("disconnect", () => onDisconnect(socket));
+  socket.on('new', (time) => newGame(socket, time));
+  socket.on('join', (gameId) => joinGame(socket, gameId));
+  socket.on('start', () => startGame(socket));
+  socket.on('lost', () => lostGame(socket));
+  socket.on('disconnect', () => onDisconnect(socket));
 };
 
-io.on("connection", onConnection);
+io.on('connection', onConnection);
 
-io.of("/").adapter.on("delete-room", (room) => {
+io.of('/').adapter.on('delete-room', (room) => {
   delete rooms[room];
 });
