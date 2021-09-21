75468c15242ddfd5848ea2ede8f42b2d67a1bac2
fifi98 <bel.filip@icloud.com>
2021-09-21 20:27:49 +0000

    feat: return error if you try joining a game with
    gameId that does not exist

--- src/handlers/joinGame.ts
+++ src/handlers/joinGame.ts
@@ -4,25 +4,33 @@ import { rooms, io } from '@misc/index';
 import { ERROR } from '@ts/types';
 
 export default async (socket: Socket, gameId: string) => {
-  // Get all current players
-  const sockets = await io.in(gameId).fetchSockets();
+  try {
+    // Check if game with the provided gameId exists
+    if (!io.of('/').adapter.rooms.has(gameId)) {
+      throw new Error(ERROR.GAME_DOES_NOT_EXIST);
+    }
 
-  // Check if user with that name is already joined
-  if (sockets.find((s: RemoteSocket<any>) => s.data.user === socket.data.user)) {
-    socket.emit('exception', ERROR.USER_TAKEN);
-    return;
+    // Get all current players
+    const sockets = await io.in(gameId).fetchSockets();
+
+    // Check if user with that name is already joined
+    if (sockets.find((s: RemoteSocket<any>) => s.data.user === socket.data.user)) {
+      throw new Error(ERROR.USER_TAKEN);
+    }
+
+    // Send event that user joined the game
+    io.in(gameId).emit('joined', socket.data.user);
+
+    // Join the room
+    socket.data.gameId = gameId;
+    socket.join(gameId);
+
+    // Send back the game data
+    socket.emit('game', {
+      time: rooms[gameId].time,
+      players: sockets.map((s: RemoteSocket<any>) => s.data.user),
+    });
+  } catch (error: any) {
+    socket.emit('exception', error.message);
   }
-
-  // Send event that user joined the game
-  io.in(gameId).emit('joined', socket.data.user);
-
-  // Join the room
-  socket.data.gameId = gameId;
-  socket.join(gameId);
-
-  // Send back the game data
-  socket.emit('game', {
-    time: rooms[gameId].time,
-    players: sockets.map((s: RemoteSocket<any>) => s.data.user),
-  });
 };
--- src/ts/types.ts
+++ src/ts/types.ts
@@ -9,4 +9,5 @@ export type Rooms = {
 
 export enum ERROR {
   USER_TAKEN = 'UserTaken',
+  GAME_DOES_NOT_EXIST = 'GameDoesNotExist',
 }
