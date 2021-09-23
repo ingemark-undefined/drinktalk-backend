43b1b71d0cb5666a05a4edc36cdc357d2ce753a7
fifi98 <bel.filip@icloud.com>
2021-09-23 15:41:20 +0200

    feat: remove storage

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,16 @@
 import { Socket } from 'socket.io';
 
-import { newGame, joinGame, startGame, lostGame, onDisconnect } from '@handlers/index';
-import { rooms, io } from '@misc/index';
+import { newGame, joinGame, startGame, onDisconnect } from '@handlers/index';
+import { io } from '@misc/index';
 
 const onConnection = (socket: Socket) => {
   socket.data.user = socket.handshake.auth.user;
 
-  socket.on('game:new', (time) => newGame(socket, time));
+  socket.on('game:new', () => newGame(socket));
   socket.on('game:join', (gameId) => joinGame(socket, gameId));
-  socket.on('game:start', () => startGame(socket));
-  socket.on('game:lost', () => lostGame(socket));
+  socket.on('game:start', (time) => startGame(socket, time));
 
   socket.on('disconnect', () => onDisconnect(socket));
 };
 
 io.on('connection', onConnection);
-
-io.of('/').adapter.on('delete-room', (room) => {
-  delete rooms[room];
-});
--- src/misc/index.ts
+++ src/misc/index.ts
@@ -1,2 +1,1 @@
 export { default as io } from './server';
-export { default as rooms } from './storage';
