16bd813ef44dce04d00792becbf010a4af0c001e
fifi98 <bel.filip@icloud.com>
2021-09-23 10:54:56 +0200

    feat: add tests for join and left events

--- src/handlers/joinGame.test.js
+++ src/handlers/joinGame.test.js
@@ -4,15 +4,17 @@ const { httpServer } = require('../misc/server');
 const { ERROR } = require('../ts/types');
 
 describe('join game module', () => {
-  let clientSocket;
+  let player1, player2;
 
   beforeEach((done) => {
-    clientSocket = Client('http://localhost:4000', { auth: { user: 'player1' } });
+    player1 = new Client('http://localhost:4000', { auth: { user: 'player1' } });
+    player2 = new Client('http://localhost:4000', { auth: { user: 'player2' }, autoConnect: false });
     done();
   });
 
   afterEach(() => {
-    clientSocket.close();
+    player1.close();
+    player2.close();
   });
 
   afterAll(() => {
@@ -20,8 +22,8 @@ describe('join game module', () => {
   });
 
   test('should receive an error that gameId does not exist', (done) => {
-    clientSocket.emit('game:join', '123456');
-    clientSocket.on('exception', (error) => {
+    player1.emit('game:join', '123456');
+    player1.on('exception', (error) => {
       expect(error).toBe(ERROR.GAME_DOES_NOT_EXIST);
       done();
     });
@@ -29,14 +31,14 @@ describe('join game module', () => {
 
   test('should receive an error that user with that name is already joined', (done) => {
     // Create new game with user 'player1'
-    clientSocket.emit('game:new', 90);
-    clientSocket.on('gameId', (gameId) => {
+    player1.emit('game:new', 90);
+    player1.on('gameId', (gameId) => {
       // Join the game with user 'player1'
-      const player = new Client('http://localhost:4000', { auth: { user: 'player1' } });
-      player.emit('game:join', gameId);
-      player.on('exception', (error) => {
+      player2.auth.user = 'player1';
+      player2.connect();
+      player2.emit('game:join', gameId);
+      player2.on('exception', (error) => {
         expect(error).toBe(ERROR.USER_TAKEN);
-        player.close();
         done();
       });
     });
@@ -44,15 +46,43 @@ describe('join game module', () => {
 
   test('should successfully join the game', (done) => {
     // Create new game with user 'player1'
-    clientSocket.emit('game:new', 90);
-    clientSocket.on('gameId', (gameId) => {
+    player1.emit('game:new', 90);
+    player1.on('gameId', (gameId) => {
       // Join the game with user 'player2'
-      const player = new Client('http://localhost:4000', { auth: { user: 'player2' } });
-      player.emit('game:join', gameId);
-      player.on('game', (game) => {
+      player2.connect();
+      player2.emit('game:join', gameId);
+      player2.on('game', (game) => {
         expect(game.time).toBe(90);
         expect(game.players).toEqual(['player1', 'player2']);
-        player.close();
+        done();
+      });
+    });
+  });
+
+  test('receive joined event when other player joins', (done) => {
+    // Create new game with user 'player1'
+    player1.emit('game:new', 90);
+    player1.on('gameId', (gameId) => {
+      // Join the game with user 'player2'
+      player2.connect();
+      player2.emit('game:join', gameId);
+      player1.on('joined', (user) => {
+        expect(user).toBe('player2');
+        done();
+      });
+    });
+  });
+
+  test('receive left event when other player joins', (done) => {
+    // Create new game with user 'player1'
+    player1.emit('game:new', 90);
+    player1.on('gameId', (gameId) => {
+      // Join the game with user 'player2'
+      player2.connect();
+      player2.emit('game:join', gameId);
+      player2.on('game', () => player2.disconnect());
+      player1.on('left', (user) => {
+        expect(user).toBe('player2');
         done();
       });
     });
