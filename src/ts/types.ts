907eeb63131f4ffa61b2d503ce7ac9259495df83
fifi98 <bel.filip@icloud.com>
2021-09-24 11:32:46 +0200

    style: remove unused types

--- src/ts/types.ts
+++ src/ts/types.ts
@@ -1,11 +1,3 @@
-export type RoomData = {
-  time: number;
-};
-
-export type Rooms = {
-  [key: string]: RoomData;
-};
-
 export enum ERROR {
   USER_TAKEN = 'UserTaken',
   GAME_DOES_NOT_EXIST = 'GameDoesNotExist',
