199c84f3cd42eadadad5197c5ca5857d5ce8c963
fifi98 <bel.filip@icloud.com>
2021-09-24 11:34:10 +0200

    style: add comment

--- src/handlers/onDisconnect.ts
+++ src/handlers/onDisconnect.ts
@@ -3,5 +3,6 @@ import { Socket } from 'socket.io';
 import { io } from '@misc/index';
 
 export default (socket: Socket) => {
+  // Send other players in the game event that the user has left
   io.in(socket.data.gameId).emit('left', socket.data.user);
 };
